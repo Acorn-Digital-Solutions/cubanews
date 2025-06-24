7629d8a849f6463da33c4e9e064e9c5c791249c1
snava10 <sergio.nava89@gmail.com>
2025-06-24 23:03:51 +0100

    refactoring to encapsulate

--- cubanewsjs/cubanews-feed/src/app/cubanewsApp.ts
+++ cubanewsjs/cubanews-feed/src/app/cubanewsApp.ts
@@ -1,14 +1,28 @@
-import { Database } from "@/app/api/dataschema";
-import { Kysely, PostgresDialect } from "kysely";
+import { Database, SubscriptionsTable } from "@/app/api/dataschema";
+import { Kysely, PostgresDialect, sql } from "kysely";
 import { Pool } from "pg";
 import { createKysely } from "@vercel/postgres-kysely";
 import * as dotenv from "dotenv";
+import { xOfEachSource } from "./api/feed/feedStrategies";
+import {
+  getNewsSourceDisplayName,
+  NewsItem,
+  NewsSourceName,
+} from "./interfaces";
+import path from "path";
+import fs from "fs";
+import moment from "moment";
+import { LlamaModel } from "node-llama-cpp";
+import { CubanewsAiAssistant } from "./ai/cubanewsAiAssistant";
+import { fileURLToPath } from "url";
 
 dotenv.config({
   path: process.env.ENV_FILE ?? `.env`,
 });
 export class CubanewsApp {
   private database: Kysely<Database>;
+  private cubanewsAiAssistant: CubanewsAiAssistant | null = null;
+  private modelPath: string = "./src/local/models";
 
   constructor() {
     console.log(
@@ -40,6 +54,100 @@ export class CubanewsApp {
 
   public get getDatabase() {
     return this.database;
+  }
+
+  public async getAiAssistant(): Promise<CubanewsAiAssistant> {
+    if (this.cubanewsAiAssistant) {
+      return this.cubanewsAiAssistant;
+    }
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    console.log(__dirname);
+    const modelPath = path.join(
+      __dirname,
+      "ai/models",
+      "hf_mradermacher_Llama-3.2-3B-Instruct.Q8_0.gguf"
+    );
+    this.cubanewsAiAssistant = new CubanewsAiAssistant(modelPath);
+    await this.cubanewsAiAssistant.initialise();
+    return this.cubanewsAiAssistant;
+  }
+
+  async getFeedItems(): Promise<NewsItem[]> {
+    const latestFeedts = await this.database
+      .selectFrom("feed")
+      .select([sql`max(feed.feedts)`.as("feedts")])
+      .executeTakeFirst();
+    if (!latestFeedts?.feedts) {
+      return [];
+    }
+
+    const items = await xOfEachSource(
+      this.database,
+      latestFeedts.feedts as number,
+      1,
+      2
+    );
+    return items;
+  }
+
+  async getRecipients(): Promise<string[]> {
+    const query = sql<SubscriptionsTable>`select s.* from subscriptions s join 
+      (select max(timestamp) as ts from subscriptions 
+      group by email) x on s.timestamp=x.ts where status='subscribed'`;
+    const result = (await query.execute(this.database)).rows;
+    return result.map((r) => r.email);
+  }
+
+  getNewsSourceLogoUrl(item: NewsItem): string {
+    const newsSource = item.source;
+    switch (newsSource) {
+      case NewsSourceName.ADNCUBA:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2Fadncuba1.webp&w=48&q=75";
+      case NewsSourceName.CATORCEYMEDIO:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2F14ymedio1.jpg&w=48&q=75";
+      case NewsSourceName.CIBERCUBA:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2Fcibercuba1.png&w=48&q=75";
+      case NewsSourceName.CUBANET:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2Fcubanet2.jpeg&w=48&q=75";
+      case NewsSourceName.DIARIODECUBA:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2Fddc1.webp&w=48&q=75";
+      case NewsSourceName.ELTOQUE:
+        return "https://www.cubanews.icu/_next/image?url=%2Fsource_logos%2Feltoque.png&w=48&q=75";
+      default:
+        return "";
+    }
+  }
+
+  async getEmailBody(): Promise<string> {
+    const feed = await cubanewsApp.getFeedItems();
+
+    const templatePath = path.join(__dirname, "mail_template.html");
+    const emailTemplate = fs.readFileSync(templatePath, { encoding: "utf-8" });
+
+    const itemTemplatePath = path.join(__dirname, "news_item_template.html");
+    const itemTemplate = fs.readFileSync(itemTemplatePath, {
+      encoding: "utf-8",
+    });
+
+    var body = "";
+    moment.locale("es");
+    for (const item of feed) {
+      const itemDate = moment(item.isoDate);
+      var itemHtml = itemTemplate
+        .replace("${news_source_logo}", cubanewsApp.getNewsSourceLogoUrl(item))
+        .replace("${title}", item.title)
+        .replace("${url}", item.url)
+        .replace("${news_source_name}", getNewsSourceDisplayName(item))
+        .replace("${content}", item.content ?? "")
+        .replace("${news-date}", itemDate.format("DD-MM-YYYY hh:mm A"));
+      body += itemHtml;
+    }
+
+    const today = moment();
+    const res = emailTemplate
+      .replace("${news}", body)
+      .replace("${date}", today.format("LL"));
+    return res;
   }
 }
 
--- cubanewsjs/cubanews-feed/src/local/aiSummary.ts
+++ cubanewsjs/cubanews-feed/src/local/aiSummary.ts
@@ -1,107 +1,44 @@
-import { fileURLToPath } from "url";
-import {
-  getLlama,
-  Llama,
-  LlamaChatSession,
-  LlamaGrammar,
-  LlamaModel,
-} from "node-llama-cpp";
-import path from "path";
-import { NewsItem } from "@/app/interfaces";
-import { generateAISummary, getFeedItems } from "./mailSender";
 import cubanewsApp from "@/app/cubanewsApp";
 
-export class CubanewsAiAssistant {
-  __dirname = path.dirname(fileURLToPath(import.meta.url));
-  model: LlamaModel | undefined;
-
-  constructor() {}
-
-  async initialise() {
-    const llama = await getLlama();
-    this.model = await llama.loadModel({
-      modelPath: path.join(
-        __dirname,
-        "models",
-        "hf_mradermacher_Llama-3.2-3B-Instruct.Q8_0.gguf"
-      ),
-    });
-  }
-
-  async generateArticleSummary(fullContent: string): Promise<string> {
-    if (this.model) {
-      const context = await this.model.createContext();
-      const session = new LlamaChatSession({
-        contextSequence: context.getSequence(),
-      });
-      const summary = await session.prompt(
-        "En espanol, resumen en 100 palabras el siguiente texto: " + fullContent
-      );
-      return summary;
-    }
-    throw new Error("Ai assistant not initialised");
-  }
-}
-
-async function aiHelloMessage(): Promise<string> {
-  // Here you would typically call an AI service to generate a summary.
-  // For simplicity, we will just return a placeholder text.
-
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const llama = await getLlama();
-  const model = await llama.loadModel({
-    modelPath: path.join(
-      __dirname,
-      "models",
-      "hf_mradermacher_Llama-3.2-3B-Instruct.Q8_0.gguf"
-    ),
-  });
-  const context = await model.createContext();
-  const session = new LlamaChatSession({
-    contextSequence: context.getSequence(),
-  });
-  const message = await session.prompt(
-    "Habla en Espanol. Saluda a los usuarios"
-  );
-  return message;
-}
-
-export async function generateAiFeedSummary(
-  newsItems: NewsItem[]
-): Promise<string> {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const llama = await getLlama();
-  const model = await llama.loadModel({
-    modelPath: path.join(
-      __dirname,
-      "models",
-      "hf_mradermacher_Llama-3.2-3B-Instruct.Q8_0.gguf"
-    ),
-  });
-  const context = await model.createContext();
-  const session = new LlamaChatSession({
-    contextSequence: context.getSequence(),
-  });
-  const prompt = newsItems
-    .map(
-      (newsItem) =>
-        `{"titulo": "${newsItem.title}", "contenido": "${newsItem.content}", "url": "${newsItem.url}", "fuente":"${newsItem.source}"}`
-    )
-    .join(",");
-  console.info(prompt);
-  const summary = await session.prompt(
-    "Crea un resumen de 100 palabras de los siguientes articulos: " +
-      prompt +
-      ". Incluye el titulo y la fuente de los articulos citados."
-  );
-  return summary;
-}
-
 function main() {
-  getFeedItems().then(async (feedItems) => {
-    console.log(feedItems);
-    const summary = await generateAiFeedSummary(feedItems);
-    console.log(summary);
+  // const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  // const modelPath = path.join(
+  //   __dirname,
+  //   "models",
+  //   "hf_mradermacher_Llama-3.2-3B-Instruct.Q8_0.gguf"
+  // );
+  // const cubanewsAiAssistant = new CubanewsAiAssistant(modelPath);
+  // cubanewsAiAssistant.initialise().then(async () => {
+  //   const feedItems = await cubanewsApp.getFeedItems();
+  //   console.log(feedItems);
+  //   const summary = await cubanewsAiAssistant.generateAiFeedSummary(feedItems);
+  //   console.log("Feed summary,", summary);
+
+  //   feedItems.map(async (item) => {
+  //     item.aiSummary = await cubanewsAiAssistant.generateArticleSummary(
+  //       item.content
+  //     );
+  //   });
+  //   console.log(feedItems);
+  // });
+  cubanewsApp.getAiAssistant().then(async (cubanewsAiAssistant) => {
+    const feedItems = await cubanewsApp.getFeedItems();
+    // console.log(feedItems);
+    // const summary = await cubanewsAiAssistant.generateAiFeedSummary(feedItems);
+    // console.log("Feed summary,", summary);
+    for (const item of feedItems) {
+      const aiSummary = await cubanewsAiAssistant.generateArticleSummary(
+        item.content
+      );
+      console.log(aiSummary);
+    }
+    // const updatedFeedItems = feedItems.map(async (item) => {
+    //   const aiSummary = await cubanewsAiAssistant.generateArticleSummary(
+    //     item.content
+    //   );
+    //   return { ...item, aiSummary: aiSummary };
+    // });
+    // console.log(updatedFeedItems);
   });
 }
 
