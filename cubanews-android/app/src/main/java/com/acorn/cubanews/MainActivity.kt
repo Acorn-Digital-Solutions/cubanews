86add00965647d7f8e625a12a66cebd3ad0189f7
snava10 <sergio.nava89@gmail.com>
2025-08-25 12:31:24 +0100

    Remove search tab

--- cubanews-android/app/src/main/java/com/acorn/cubanews/MainActivity.kt
+++ cubanews-android/app/src/main/java/com/acorn/cubanews/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
