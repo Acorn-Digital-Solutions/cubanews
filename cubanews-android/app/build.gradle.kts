c9a8ce6894c88520541da6b3e149e3d61afb8b93
Sergio Navarrete <sergio.nava89@gmail.com>
2025-08-31 22:18:22 +0100

    Google play deployment (#143)

--- cubanews-android/app/build.gradle.kts
+++ cubanews-android/app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     defaultConfig {
         applicationId = "com.acorn.cubanews"
         minSdk = 30
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
