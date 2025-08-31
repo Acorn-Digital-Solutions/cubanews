5efa9723edeab25ea739759bef1d17c07a753954
snava10 <sergio.nava89@gmail.com>
2025-08-31 22:16:46 +0100

    target android 36

--- cubanews-android/app/build.gradle.kts
+++ cubanews-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.acorn.cubanews"
         minSdk = 30
-        targetSdk = 35
-        versionCode = 1
+        targetSdk = 36
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
