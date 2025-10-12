ec942e9f532b5c3e9c736d3215c6d2c2f67bd793
snava10 <sergio.nava89@gmail.com>
2025-10-12 18:41:41 +0100

    Increase version

--- cubanews-android/app/build.gradle.kts
+++ cubanews-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.acorn.cubanews"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
