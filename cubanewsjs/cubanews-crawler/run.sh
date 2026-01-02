034bd742f4ed55f30096b7b7e0d5f133a410d02a
snava10 <sergio.nava89@gmail.com>
2026-01-02 15:52:10 +0000

    preserving logs

--- cubanewsjs/cubanews-crawler/run.sh
+++ cubanewsjs/cubanews-crawler/run.sh
@@ -1,12 +1,15 @@
 
+#!/bin/bash
+set -e
+
 echo "$(date) Refreshing feed Locally" > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/ddc-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/adncuba-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/cubanet-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/eltoque-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
-cd $HOME/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local > $HOME/cubanews-crawler.log 2>&1;
-npm run start:mail > $HOME/cubanews-crawler.log 2>&1;
-npm run start:postToFB:playwright > $HOME/cubanews-crawler.log 2>&1;
-echo "$(date) Completed Refreshing feed Locally" > ~/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/ddc-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/adncuba-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/cubanet-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/eltoque-crawler && npm run start >> $HOME/cubanews-crawler.log 2>&1;
+cd $HOME/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local >> $HOME/cubanews-crawler.log 2>&1;
+npm run start:mail >> $HOME/cubanews-crawler.log 2>&1;
+npm run start:postToFB:playwright >> $HOME/cubanews-crawler.log 2>&1;
+echo "$(date) Completed Refreshing feed Locally" >> $HOME/cubanews-crawler.log 2>&1;
