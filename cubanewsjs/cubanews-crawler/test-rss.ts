c1f4c962ee263b3024b7def7c60db6fd5b533f8b
snava10 <sergio.nava89@gmail.com>
2026-01-03 23:48:41 +0000

    cibercuba rss crawler

--- cubanewsjs/cubanews-crawler/test-rss.ts
+++ cubanewsjs/cubanews-crawler/test-rss.ts
@@ -1,22 +1,10 @@
-import { NewsSource, NewsSourceName } from "./src/newsSource.js";
-import { CubanewsRSSCrawler } from "./src/cubanewsRRSCrawler.js";
+import {
+  CatorceYMedioRSSCrawler,
+  CibercubaRSSCrawler,
+} from "./src/cubanewsRRSCrawler.js";
 
 async function testRSS() {
-  const sources = [
-    {
-      name: NewsSourceName.CATORCEYMEDIO,
-      startUrls: new Set(["https://www.14ymedio.com/cuba"]),
-      rssFeed: "https://www.14ymedio.com/rss/",
-      datasetName: NewsSourceName.CATORCEYMEDIO + "-dataset",
-    } as NewsSource,
-    {
-      name: NewsSourceName.DIRECTORIO_CUBANO,
-      startUrls: new Set(),
-      rssFeed: "https://www.directoriocubano.info/feed/",
-      datasetName: NewsSourceName.DIRECTORIO_CUBANO + "-dataset",
-    } as NewsSource,
-  ];
-  const crawler = new CubanewsRSSCrawler(sources);
+  const crawler = new CibercubaRSSCrawler();
   await crawler.getRSSContent(false);
 }
 
